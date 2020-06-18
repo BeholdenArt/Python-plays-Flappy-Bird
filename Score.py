6fb7a5e886e98c3352900486723e8c1c0e684d6b
BeholdenArt <50458157+BeholdenArt@users.noreply.github.com>
2020-06-18 23:51:15 +0530

    Added Score File
    Contains Score class that has all functions to show, increament score and also show generation count

---
+++ Score.py
@@ -0,0 +1,23 @@
+import pygame
+pygame.init()
+
+
+class Score():
+
+    def __init__(self):
+        self.count = 0
+        self.font = pygame.font.Font("CHICKEN Pie.ttf", 32)
+        self.text_x, self.text_y = 10, 10
+        self.gen_x, self.gen_y = 400, 10
+
+
+    def display_score(self, win):
+        self.class_score = self.font.render("SCORE : " + str(self.count), True, (255, 0, 0))
+        win.blit(self.class_score, (self.text_x, self.text_y))
+
+    def update_score(self):
+        self.count += 1
+
+    def show_generation(self, generation, win):
+        self.generation_counter = self.font.render("GEN : " + str(generation), True, (255, 0, 0))
+        win.blit(self.generation_counter, (self.gen_x, self.gen_y))
